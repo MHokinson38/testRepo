ddc609d7150eebaac781db1cc8975ce82d18d768
Matthew Hokinson <Mr.Awesome@matthews-air.dhcp.fnal.gov>
2018-07-13 11:31:13 -0500

    added modNewFile.C

---
+++ modNewFile.C
@@ -0,0 +1,15 @@
+//Test
+//At least this is kind of working
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+
+	std::cout << "Look I made a change to the file!\n";
+
+	std::cout << "look math ! xD" << 5 + 7 << std::endl;
+
+	return 0;
+}
